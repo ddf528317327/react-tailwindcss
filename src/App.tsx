49d3c33164b2409969dd25814783148e11465d6e
Dongfang Deng <528317327@qq.com>
2022-03-09 10:04:36 +0800

    feat:格式化代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center text-3xl font-bold">
-        Hello world!
-      </h1>
+      <h1 className="text-center text-3xl font-bold">Hello world!</h1>
     </div>
   );
 }
