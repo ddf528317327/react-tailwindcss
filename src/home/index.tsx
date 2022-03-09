69f8f2687ac8a141ade9b9aa6c80803f4d6d8d89
Dongfang Deng <528317327@qq.com>
2022-03-09 16:09:57 +0800

    feat: add .eslintrc

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -14,6 +14,32 @@ function Home() {
           <p className="text-slate-500">You have a new message!</p>
         </div>
       </div>
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div className="md:flex">
+          <div className="md:shrink-0">
+            <img
+              className="h-48 w-full object-cover md:h-full md:w-48"
+              src={logo}
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Case study
+            </div>
+            <a
+              href="#"
+              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Finding customers for your new business
+            </a>
+            <p className="mt-2 text-slate-500">
+              Getting a new business off the ground is a lot of hard work. Here
+              are five ideas you can use to find your first customers.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
